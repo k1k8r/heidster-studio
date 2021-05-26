df8ca11a4bc38919b4bc61b5d5681aa9052fcbe1
k1k8r <andreyzadonsky02@mail.ru>
2021-05-26 14:01:57 +0300

    Created header copmonent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core';
+import { LayoutModule } from './layout';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    LayoutModule,
     AppRoutingModule,
   ],
   providers: [],
